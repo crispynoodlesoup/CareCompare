19be3545860a9b010d2e9614712e4814296f0880
crispynoodlesoup <thepanman321@mgmail.com>
2024-02-18 10:28:36 -0600

    final touches~

--- src/components/priceChecker/priceChecker.jsx
+++ src/components/priceChecker/priceChecker.jsx
@@ -114,28 +114,30 @@ function Analysis({ data }) {
           Find Nearby Attorneys
         </button>
       </div>
-      <div className={style.tableWrapper}>
-        <table className={style.itemTable2}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            {attorneys.map((attorney) => {
-              return (
-                <tr key={attorney.id}>
-                  <td>{attorney.name}</td>
-                  <td>{attorney.email}</td>
-                  <td>{attorney.full_address}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      {attorneys.length > 0 ? (
+        <div className={style.tableWrapper}>
+          <table className={style.itemTable2}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {attorneys.map((attorney) => {
+                return (
+                  <tr key={attorney.id}>
+                    <td>{attorney.name}</td>
+                    <td>{attorney.email}</td>
+                    <td>{attorney.full_address}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
     </>
   );
 }
