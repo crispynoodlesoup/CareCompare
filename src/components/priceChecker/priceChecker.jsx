0f99f4a286a6b6f3743d7746764b82f7d1f80b54
crispynoodlesoup <thepanman321@mgmail.com>
2024-02-17 18:19:51 -0600

    file upload UI and text confirmation

--- src/components/priceChecker/priceChecker.jsx
+++ src/components/priceChecker/priceChecker.jsx
@@ -1,6 +1,10 @@
 import style from "./priceChecker.module.css";
+import { useState } from "react";
 
 function PriceChecker() {
+  const [file, setFile] = useState(null);
+  if (file) console.log(file.name);
+
   return (
     <main>
       <div className={style.content}>
@@ -13,10 +17,29 @@ function PriceChecker() {
           your medical bill, and submit it here for an analysis.
         </p>
         <h5>*Please upload a UB-04 document in a PNG or JPEG format</h5>
-        <div className={style.fileButtons}>
-          <button>UPLOAD</button>
-          <button>SUBMIT</button>
-        </div>
+        <form className={style.fileButtons}>
+          <label htmlFor="upload" className={style.fileButton}>
+            UPLOAD
+          </label>
+          <input
+            id="upload"
+            accept=".jpg, .jpeg, .png"
+            type="file"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          {file ? (
+            <button type="button" className={style.fileButton}>
+              SUBMIT
+            </button>
+          ) : (
+            <button type="button" className={style.fileButton} disabled>
+              SUBMIT
+            </button>
+          )}
+        </form>
+        {file ? (
+          <p className={style.selectedFile}>{`Selected file: ${file.name}`}</p>
+        ) : null}
       </div>
     </main>
   );
