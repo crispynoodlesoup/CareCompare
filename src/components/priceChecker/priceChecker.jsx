cbb62b2261976ebab5a442bcfa030d2060ddb5cb
crispynoodlesoup <thepanman321@gmail.com>
2024-02-21 03:15:36 -0600

    change text for paragraph balance

--- src/components/priceChecker/priceChecker.jsx
+++ src/components/priceChecker/priceChecker.jsx
@@ -87,7 +87,7 @@ function PriceChecker() {
         <p>
           Our automatic price comparison tool is here to make your care
           affordable, transparent, and simple! We'll ensure you're getting the
-          best prices for any and all healthcare products and services you're
+          best prices for any healthcare products and services you're
           currently paying for. It's simple, just request an itemized receipt of
           your medical bill, and submit it here for an analysis.
         </p>
