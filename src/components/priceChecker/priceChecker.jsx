04e9589e8fed505e2602c188d958ccfed3f6263a
crispynoodlesoup <thepanman321@gmail.com>
2024-02-21 06:16:54 -0600

    create usable examples

--- src/components/priceChecker/priceChecker.jsx
+++ src/components/priceChecker/priceChecker.jsx
@@ -10,10 +10,13 @@ function PriceChecker() {
   const isMounted = useRef(false);
   const [file, setFile] = useState(null);
   const [submit, setSubmit] = useState({ count: 0 });
+  const [showExamples, setShowExamples] = useState(false);
+  const [selectExample, setSelectExample] = useState(false);
   const [loadingAnalysis, setLoadingAnalysis] = useState(false);
   const [loadingAttorneys, setLoadingAttorneys] = useState(false);
   const [data, setData] = useState(null);
   const [attorneys, setAttorneys] = useState(null);
+  console.log(file);
 
   const handleGetAttorneys = async () => {
     try {
@@ -31,7 +34,7 @@ function PriceChecker() {
             .catch((error) => {
               console.error("Error fetching attorneys: " + error);
             })
-            .finally( () => {
+            .finally(() => {
               setLoadingAttorneys(false);
             });
         },
@@ -66,11 +69,37 @@ function PriceChecker() {
           setData(data);
         })
         .catch((error) => console.log(error))
-        .finally( () => {
+        .finally(() => {
           setLoadingAnalysis(false);
         });
     }
   }, [isMounted, submit]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      let example;
+      let type;
+      switch (selectExample) {
+        case "0":
+          setFile(null);
+          return;
+        case "1":
+          example = "ex3.jpg";
+          type = "jpg";
+          break;
+        case "2":
+          example = "target.png";
+          type = "png";
+          break;
+      }
+      fetch(example)
+        .then((response) => response.blob())
+        .then((data) => {
+          let exampleFile = new File([data], example, { type });
+          setFile(exampleFile);
+        });
+    }
+  }, [selectExample]);
 
   useEffect(() => {
     isMounted.current = true;
@@ -87,11 +116,32 @@ function PriceChecker() {
         <p>
           Our automatic price comparison tool is here to make your care
           affordable, transparent, and simple! We'll ensure you're getting the
-          best prices for any healthcare products and services you're
-          currently paying for. It's simple, just request an itemized receipt of
-          your medical bill, and submit it here for an analysis.
+          best prices for any healthcare products and services you're currently
+          paying for. It's simple, just request an itemized receipt of your
+          medical bill, and submit it here for an analysis.
         </p>
-        <h5>*Please upload a UB-04 document in a PNG or JPEG format</h5>
+        <h5>
+          Please upload a UB-04 document in a PNG/JPEG format or{" "}
+          <a
+            className={style.examplesButton}
+            onClick={() => setShowExamples(!showExamples)}
+          >
+            view examples
+          </a>
+        </h5>
+        {showExamples ? (
+          <select
+            name="examples"
+            id="exampleSelect"
+            className={style.exampleSelect}
+            value={selectExample}
+            onChange={(e) => setSelectExample(e.target.value)}
+          >
+            <option value="0">Select an example</option>
+            <option value="1">Example 1</option>
+            <option value="2">Example 2</option>
+          </select>
+        ) : null}
         {file ? (
           <>
             <img
@@ -112,7 +162,10 @@ function PriceChecker() {
             id="upload"
             accept=".jpg, .jpeg, .png"
             type="file"
-            onChange={(e) => setFile(e.target.files[0])}
+            onChange={(e) => {
+              setFile(e.target.files[0]);
+              setShowExamples(false);
+            }}
           />
           {file ? (
             <button
@@ -128,7 +181,7 @@ function PriceChecker() {
             </button>
           )}
         </form>
-        { loadingAnalysis ? <p className={style.loading}>Loading...</p> : null}
+        {loadingAnalysis ? <p className={style.loading}>Loading...</p> : null}
         {data ? <Analysis data={data} /> : null}
 
         <h2 className={style.sectionTitle}>Attorneys Near You</h2>
@@ -142,8 +195,14 @@ function PriceChecker() {
             Find Nearby Attorneys
           </button>
         </div>
-        { loadingAttorneys ? <p className={style.loading}>Loading...</p> : null}
-        { attorneys ? attorneys.length > 0 ? <AttorniesTable attorneys={attorneys}/> : <p>Could not find attorneys in your location {":("}</p> : null }
+        {loadingAttorneys ? <p className={style.loading}>Loading...</p> : null}
+        {attorneys ? (
+          attorneys.length > 0 ? (
+            <AttorniesTable attorneys={attorneys} />
+          ) : (
+            <p>Could not find attorneys in your location {":("}</p>
+          )
+        ) : null}
       </div>
     </main>
   );
