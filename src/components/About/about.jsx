9ff02bee1d201ceedc7cc81bfb3cbf93036cd034
crispynoodlesoup <thepanman321@gmail.com>
2024-02-21 22:34:37 -0600

    add links to our github

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -6,11 +6,13 @@ function About() {
       <div className={style.content}>
         <h1 className={style.sectionTitle}>About CareCompare</h1>
         <p>
-          CareCompare is a 24-hour project done by Rafael Padilla and Peyton
-          Barre for the Axxess 2024 Hackathon. We've since polished it a little
-          more and hosted it online as a portfolio project. Keep in mind this is
-          not meant to be a complete product, and is being hosted using free
-          services! 
+          CareCompare is a 24-hour project done by{" "}
+          <a href="https://github.com/crispynoodlesoup" target="_blank">Rafael Padilla</a> and{" "}
+          <a href="https://github.com/peytonbarre" target="_blank">Peyton Barre</a> for the
+          Axxess 2024 Hackathon. We've since polished it a little more and
+          hosted it online as a portfolio project. Keep in mind this is not
+          meant to be a complete product, and is being hosted using free
+          services!
         </p>
       </div>
     </main>
